d0a6b4a1b2d58a8f06e2fb09bb2da8a1254eb369
Aman <aman@Amans-MacBook-Air.local>
2025-06-22 18:36:33 +0530

    index.html

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-document.querySelector(".check").addEventListener("click", function(event) {
+let secretNumber = Math.floor(Math.random()* 20) +1;
+let score = 20;
+let highscore = 0;
 
-    let numGen = Math.floor(Math.random() * 20) + 1;
-    let input = document.querySelector("#inputText").value;
-    let box= document.querySelector(".box").innerHTML = numGen;
+// Selectors for the elements in the HTML
+const input = document.getElementById("inputText");
+const box = document.querySelector(".box");
+const message = document.querySelector(".message");
+const scoreDisplay = document.querySelector('.score');
+const highscoreDisplay = document.querySelector(".highscore");
 
+// to display Message 
+function displayMessage(msg){
+    message.textContent = msg;
+}
 
-    if (input === "") {
-        document.querySelector(".enter").innerHTML = "Please enter a number" ;
-        document.querySelector(".box").display = "none";
-    }else if (Number(input) === numGen ){
-       document.querySelector(".zero").innerHTML = Number(10)  ;
-    } 
-
+// reset game
+document.querySelector(".again").addEventListener("click", function(){
+    secretNumber = Math.floor(Math.random()*20) +1;
+    box.textContent = "?";
+    box.style.backgroundColor = "black";
+    input.value = "";
+    score = 20;
+    displayMessage("");
+    scoreDisplay.textContent = score;
 });
 
+// check user input
+document.querySelector(".check").addEventListener("click", function(){
+     const guess = Number(input.value);
 
-document.querySelector(".again").addEventListener("click", function(event) {
-    document.querySelector("")
-});
+      if (!guess){
+        displayMessage("Please enter a number");
+       } else if (guess===secretNumber){
+        displayMessage("🎉 Correct Number!");
+        box.textContent = secretNumber;
+        box.style.backgroundColor = 'green';
 
+        if(score > highscore){
+            highscore = score;
+            highscoreDisplay.textContent = highscore;
+        }
+       } else {
+        if (score > 1 ){
+            displayMessage(guess > secretNumber ? "Too high" : "Too Low");
+            score--;
+            scoreDisplay.textContent = score;
+        } else {
+            displayMessage("Game Over!");
+            scoreDisplay.textContent = 0;
+        }
+       } 
+
+}); 
